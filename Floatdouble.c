c0d7efdedfb79e4b70ec7034bc0f10097a937322
Anees Sanjana <aneessanjanap@gmail.com>
2024-11-26 15:03:39 +0530

    Create Floatdouble.c

---
+++ Floatdouble.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  float num1;
+  double num2;
+  printf(" enter a number:");
+  scanf("%f",&num1);
+  printf("enter another number:");
+  scanf("%If",&num2);
+  printf("num1=%f\n",num1);
+  printf("num2=%If",num2);
+  return0;
+}
