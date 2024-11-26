b2406b48c9cbe87e5f54aa60e4b30e303b83eda2
Anees Sanjana <aneessanjanap@gmail.com>
2024-11-26 12:48:10 +0530

    Create Greatest of two numbers.c

---
+++ Greatest of two numbers.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main(void){
+    int num1,num2;
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
+    if (num1>num2){
+        printf("%d is greater than %d");
+        printf("%d is greater ./n"
+        ");
+            }else{}
+    printf("%d is greater./n", num 2)
+}
