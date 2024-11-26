c8ee275120bfc04f7c27084a9d0bf29a416d8adb
Anees Sanjana <aneessanjanap@gmail.com>
2024-11-26 15:07:47 +0530

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  printf("Enter the value of n:");
+scanf("%d",&n);
+  while(i<=n){
+   sum+=i:
+    i++;
+  }
+  printf("sum of first %d natural number is %d\n",n,sum);
+  retun 0;
+}
