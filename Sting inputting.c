17c42fd12beaf1a3cd581b87d5a94a7c9b1807b9
Anees Sanjana <aneessanjanap@gmail.com>
2024-11-29 08:38:41 +0530

    Update Sting inputting.c

--- Sting inputting.c
+++ Sting inputting.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    
+    // Reading a string with spaces using fgets
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    printf("You entered: %s\n", str);
+    
+    return 0;
+}
 
