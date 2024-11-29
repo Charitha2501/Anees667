92c58451677519d303d9edc9d5d078f654697ac9
Anees Sanjana <aneessanjanap@gmail.com>
2024-11-29 08:37:36 +0530

    Create String initialisation declaration.c

---
+++ String initialisation declaration.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing a string
+    char str1[] = "Hello, World!"; // Implicitly includes the null character
+    printf("String: %s\n", str1);  // Output: Hello, World!
+    
+    return 0;
+}
